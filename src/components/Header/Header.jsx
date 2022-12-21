a4f41b7b7ce9c3aa800574b2b69c897c97cc7d9f
SivetsStanislav <sivetsus@gmail.com>
2022-12-21 19:07:31 +0300

    done header image

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.scss'
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header__container'>
+                <div className='image-container'>
+                    <div className='image'>
+        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
